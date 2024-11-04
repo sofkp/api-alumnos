4e4668d3b65704e6827bfb0e70ca11e0f4abccc7
Sofía Ku <165909907+sofkp@users.noreply.github.com>
2024-11-03 21:15:00 -0500

    Update modificarAlumno.py

--- modificarAlumno.py
+++ modificarAlumno.py
@@ -2,8 +2,8 @@ import boto3
 
 def lambda_handler(event, context):
     # Entrada (json)
-    tenant_id = event['tenant_id']
-    alumno_id = event['alumno_id']
+    tenant_id = event['body']['tenant_id']
+    alumno_id = event['body']['alumno_id']
     alumno_datos = event['alumno_datos']
     # Proceso
     dynamodb = boto3.resource('dynamodb')
